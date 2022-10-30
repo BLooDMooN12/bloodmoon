46125bd86972c1584493c3cc871a21bc71163c4e
BLooDMooN12 <117015168+BLooDMooN12@users.noreply.github.com>
2022-10-30 14:37:54 +0300

    Create ddd.py

---
+++ ddd.py
@@ -0,0 +1,23 @@
+import os 
+from cryptography.fernet import Fernet
+
+dosyalar = []
+
+for dosya in os.listdir():
+    if dosya == "coz.py" or dosya == "anahtar.key":
+        continue
+    if os.path.isfile(dosya):
+        dosyalar.append(dosya)
+print(dosyalar)
+
+with open("anahtar.key","rb") as anahtar_dosya:
+    anahtar_dosya.read()
+print(anahtar)
+
+for dosya in dosyalar:
+    with open(dosya,"rb") as okunan_dosya:
+        icerik = okunan_dosya.read()
+    cozulen_icerik = Fernet(anahtar).decrypt(icerik)
+    with open(dosya,"wb") as cozulen_dosya:
+        cozulen_dosya.write(cozulen_icerik)
+
